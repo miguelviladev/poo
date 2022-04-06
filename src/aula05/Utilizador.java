421d6d6ddc1d303d7c468f7d82d3376cb725cc2e
Miguel Vila <miguel.vila.dev@gmail.com>
2022-04-07 00:01:09 +0100

    Exec4

---
+++ src/aula05/Utilizador.java
@@ -0,0 +1,35 @@
+package aula05;
+
+public class Utilizador {
+    private String nome, curso; private int n_mec;
+
+    Utilizador(String nome, int n_mec, String curso) {
+        this.setNome(nome);
+        this.setNumMec(n_mec);
+        this.setCurso(curso);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNumMec(int n_mec) {
+        this.n_mec = n_mec;
+    }
+
+    public int getNumMec() {
+        return this.n_mec;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return this.curso;
+    }
+}
