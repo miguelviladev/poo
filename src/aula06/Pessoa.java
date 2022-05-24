d086c9f2795a8175e9db3a28633e6b7ac29835f5
Miguel Vila <miguel.vila.dev@gmail.com>
2022-05-24 02:43:35 +0100

    Pessoa

--- src/aula06/Pessoa.java
+++ src/aula06/Pessoa.java
@@ -6,7 +6,7 @@ public class Pessoa {
     private int cartao_cidadao;
     private Data data_nascimento;
 
-    Pessoa(String nome, int cartao_cidadao, Data data_nascimento) {
+    public Pessoa(String nome, int cartao_cidadao, Data data_nascimento) {
         this.setNome(nome);
         this.setCartaoCidadao(cartao_cidadao);
         this.setDataNascimento(data_nascimento);
@@ -43,5 +43,26 @@ public class Pessoa {
     @Override
     public String toString() {
         return this.getNome() + "; CC: " + this.getCartaoCidadao() + "; Data de Nascimento: " + this.getDataNascimento().getData();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if ((this.nome == null) ? (other.nome != null) : !this.nome.equals(other.nome)) {
+            return false;
+        }
+        if (this.cartao_cidadao != other.cartao_cidadao) {
+            return false;
+        }
+        if (this.data_nascimento != other.data_nascimento && (this.data_nascimento == null || !this.data_nascimento.equals(other.data_nascimento))) {
+            return false;
+        }
+        return true;
     }
 }
